1e5c3941e3f9d1f4c8d21d1a161ad8887cb57949
Rahul <patilr2812@gmail.com>
2026-09-05 17:46:11 +0530

    fix:click on gmail icon page is open.

--- resources/views/ourteam/ourteam.blade.php
+++ resources/views/ourteam/ourteam.blade.php
@@ -23,56 +23,56 @@
             </div>
         </section>
 
-            <!-- team start -->
-            <section class="team-area pt-50 pb-40 gray-bg">
-                <div class="container">
-                    @foreach($department as $dkey => $dvalue)
-                        <div class="section-title text-center">
-                            <h2><span>{{ $dvalue->department_name }}</span></h2>
-                        </div>
-                        <div class="row justify-content-center">
-                            @foreach($dvalue->ourteam as $okey => $ovalue)
-                                <div class="col-xl-3 col-lg-3 col-md-6 d-flex">
-                                    <div class="single-team text-center mb-30 w-100">
-                                        <div class="team-thumb">
-                                            @if($ovalue['profile_image'])
-                                                <img src="{{ asset('storage/'.$ovalue['profile_image']) }}" class="img-fluid" alt="">
-                                            @else
-                                                <img src="{{ asset('default_images/user.jpg') }}" class="img-fluid" alt="">
-                                            @endif
-                                            <div class="team-icon">
-                                                <a href="{{ $ovalue['facebook'] ? $ovalue['facebook'] : 'javascript:void(0)' }}" {{ $ovalue['facebook'] ? "target='_blank'" : ''}}><i class="fab fa-facebook-f"></i></a>
-                                                <a href="{{ $ovalue['linkedin'] ? $ovalue['linkedin'] : 'javascript:void(0)' }}" {{ $ovalue['linkedin'] ? 'target="_blank"' : ''}}><i class="fab fa-linkedin"></i></a>
-                                                <a href="{{ $ovalue['email'] ? $ovalue['email'] : 'javascript:void(0)' }}" {{ $ovalue['email'] ? 'target="_blank"' : ''}}><i class="fab fa-google-plus-g"></i></a>
-                                                <a href="{{ $ovalue['instagram'] ? $ovalue['instagram'] : 'javascript:void(0)' }}" {{ $ovalue['instagram'] ? 'target="_blank"' : ''}}><i class="fab fa-instagram"></i></a>
-                                            </div>
-                                        </div>
-                                        <div class="team-content text-center d-flex flex-column align-items-center">
-                                            <h3>{{ $ovalue['name'] }}</h3>
-                                            <h5 class="h5-title text-center">{{ $ovalue['designation']['designation_name'] }}</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                            <!-- <div class="col-xl-3 col-lg-3 col-md-6">
-                                <div class="single-team text-center mb-30">
+        <!-- team start -->
+        <section class="team-area pt-50 pb-40 gray-bg">
+            <div class="container">
+                @foreach($department as $dkey => $dvalue)
+                    <div class="section-title text-center">
+                        <h2><span>{{ $dvalue->department_name }}</span></h2>
+                    </div>
+                    <div class="row justify-content-center">
+                        @foreach($dvalue->ourteam as $okey => $ovalue)
+                            <div class="col-xl-3 col-lg-3 col-md-6 d-flex">
+                                <div class="single-team text-center mb-30 w-100">
                                     <div class="team-thumb">
-                                        <img src="img/team/team-02.jpg" alt="image_not_found">
+                                        @if($ovalue['profile_image'])
+                                            <img src="{{ asset('storage/'.$ovalue['profile_image']) }}" class="img-fluid" alt="">
+                                        @else
+                                            <img src="{{ asset('default_images/user.jpg') }}" class="img-fluid" alt="">
+                                        @endif
                                         <div class="team-icon">
-                                            <a href="javascript:void(0);"><i class="fab fa-facebook-f"></i></a>
-                                            <a href="javascript:void(0);"><i class="fab fa-twitter"></i></a>
-                                            <a href="javascript:void(0);"><i class="fab fa-google-plus-g"></i></a>
-                                            <a href="javascript:void(0);"><i class="fab fa-instagram"></i></a>
+                                            <a href="{{ $ovalue['facebook'] ? $ovalue['facebook'] : 'javascript:void(0)' }}" {{ $ovalue['facebook'] ? "target='_blank'" : ''}}><i class="fab fa-facebook-f"></i></a>
+                                            <a href="{{ $ovalue['linkedin'] ? $ovalue['linkedin'] : 'javascript:void(0)' }}" {{ $ovalue['linkedin'] ? 'target="_blank"' : ''}}><i class="fab fa-linkedin"></i></a>
+                                            <a href="{{ $ovalue['email'] ? 'mailto:'.$ovalue['email'] : 'javascript:void(0)' }}" {{ $ovalue['email'] ? 'target="_blank"' : ''}}><i class="fab fa-google-plus-g"></i></a>
+                                            <a href="{{ $ovalue['instagram'] ? $ovalue['instagram'] : 'javascript:void(0)' }}" {{ $ovalue['instagram'] ? 'target="_blank"' : ''}}><i class="fab fa-instagram"></i></a>
                                         </div>
                                     </div>
-                                    <div class="team-content text-center">
-                                        <h3>Janimiya</h3>
-                                        <h5>Sales</h5>
+                                    <div class="team-content text-center d-flex flex-column align-items-center">
+                                        <h3>{{ $ovalue['name'] }}</h3>
+                                        <h5 class="h5-title text-center">{{ $ovalue['designation']['designation_name'] }}</h5>
                                     </div>
                                 </div>
-                            </div> -->
-                        </div>
-                    @endforeach
-                </div>
-            </section>
+                            </div>
+                        @endforeach
+                        <!-- <div class="col-xl-3 col-lg-3 col-md-6">
+                            <div class="single-team text-center mb-30">
+                                <div class="team-thumb">
+                                    <img src="img/team/team-02.jpg" alt="image_not_found">
+                                    <div class="team-icon">
+                                        <a href="javascript:void(0);"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="javascript:void(0);"><i class="fab fa-twitter"></i></a>
+                                        <a href="javascript:void(0);"><i class="fab fa-google-plus-g"></i></a>
+                                        <a href="javascript:void(0);"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                </div>
+                                <div class="team-content text-center">
+                                    <h3>Janimiya</h3>
+                                    <h5>Sales</h5>
+                                </div>
+                            </div>
+                        </div> -->
+                    </div>
+                @endforeach
+            </div>
+        </section>
     @endsection
